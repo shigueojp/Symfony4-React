32c2741aeb4612ba7f621dc33d3c3b826638226c
Victor Okuhama <victor.okuhama@hypnobox.com.br>
2018-12-18 11:15:54 -0200

    Update V.1.1

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BlogPost;
+use App\Entity\Comment;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * @var
+     */
+    private $faker;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->faker = Factory::create();
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
+        $manager->flush();
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
+        for($i = 0; $i < 100; $i++){
+            $blogPost = new BlogPost();
+            $blogPost->setTitle($this->faker->realText(30));
+            $blogPost->setPublished($this->faker->dateTime);
+            $blogPost->setContent($this->faker->realText());
+            $blogPost->setAuthor($user);
+            $blogPost->setSlug($this->faker->slug);
+
+            $this->setReference("blog_post_$i", $blogPost);
+
+            $manager->persist($blogPost);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+        for($i = 0; $i < 100; $i++){
+            for ($j = 0; $j < rand(1,10); $j++){
+                $comment = new Comment();
+                $comment->setContent($this->faker->realText());
+                $comment->setPublished($this->faker->dateTimeThisYear());
+                $comment->setAuthor($this->getReference('user_admin'));
+                $comment->setBlogPost($this->getReference("blog_post_$i"));
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@glob.com');
+        $user->setName('Admin');
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'Hypno123'
+        ));
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/BlogPost.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiResource()
+ */
+class BlogPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $published;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="blogPost")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private  $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublished(): ?\DateTimeInterface
+    {
+        return $this->published;
+    }
+
+    public function setPublished(\DateTimeInterface $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug($slug): void
+    {
+        $this->slug = $slug;
+    }
+
+
+}
